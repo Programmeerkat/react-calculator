320c61a2152f4fa628129c0c09088def00ba2ec8
unknown <danieltennapel@gmail.com>
2024-06-12 23:46:30 +0200

    refactored component logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState, useRef, forwardRef } from 'react'
 
 const BUTTONS = [
-  { type: "number", keyName: 'number1', label: '1', keyPress: '1'},
+  { type: "number", keyName: 'number1', label: '1', keyPress: '1' },
   { type: "number", keyName: 'number2', label: '2', keyPress: '2' },
   { type: "number", keyName: 'number3', label: '3', keyPress: '3' },
   { type: "number", keyName: 'number4', label: '4', keyPress: '4' },
@@ -19,7 +19,7 @@ const BUTTONS = [
   { type: "operation", keyName: 'evaluate', label: '=', keyPress: 'Enter' },
   { type: "operation", keyName: 'clear', label: 'c', keyPress: 'c' },
   { type: "operation", keyName: 'delete', label: '←', keyPress: 'Backspace' },
-];
+]
 
 const Button = forwardRef(function Button({ keyName, label, onClick }, ref) {
   return (
@@ -31,7 +31,7 @@ const Button = forwardRef(function Button({ keyName, label, onClick }, ref) {
     >
       {label}
     </button>
-  );
+  )
 })
 
 function Screen({ value }) {
@@ -39,11 +39,11 @@ function Screen({ value }) {
     <div className="screen">
       <span>{value}</span>
     </div>
-  );
+  )
 }
 
 export default function App() {
-  const [query, setQuery] = useState([])
+  const [inputHistory, setInputHistory] = useState([])
 
   const buttonRefs = useRef({})
 
@@ -54,44 +54,45 @@ export default function App() {
   }, [])
 
   const handleKeyPress = (key) => {
-    const buttonRef = buttonRefs.current[key];
+    const buttonRef = buttonRefs.current[key]
     if (buttonRef === undefined) {
       return
     }
     buttonRef.click()
-  };
+  }
 
-  const handleClick = ({type, keyName, label}) => {
+  const handleClick = ({ type, keyName, label }) => {
+
     if (keyName === 'evaluate') {
-      setQuery([{type: 'number', label: evaluateResult(), result: true}])
+      setInputHistory([{type: 'number', label: evaluateResult(), result: true}])
       return
     }
     if (keyName === 'clear') {
-      setQuery([])
+      setInputHistory([])
       return
     }
     if (keyName === 'delete') {
-      setQuery((prev) => prev.slice(0, -1))
+      setInputHistory((prev) => prev.slice(0, -1))
       return
     }
     if (keyName === 'multiply' || keyName === 'divide' || keyName === 'add') {
-      if (query.length === 0 || query[query.length - 1].type === 'operation') {
+      if (inputHistory.length === 0 || inputHistory[inputHistory.length - 1].type === 'operation') {
         return
       }
     }
     if (keyName === 'subtract') {
-      if (query[query.length - 1]?.keyName === 'subtract') {
+      if (inputHistory[inputHistory.length - 1]?.keyName === 'subtract') {
         return
       }
     }
     if (keyName === 'dot') {
-      if (query.length === 1 && query[0].result) {
-        setQuery([])
+      if (inputHistory.length === 1 && inputHistory[0].result) {
+        setInputHistory([])
       }
       let seenDot = false
       let seenOperator = false
       let dotIsValid = true
-      query.slice().reverse().forEach((item) => {
+      inputHistory.slice().reverse().forEach((item) => {
         if (item.keyName === 'dot') {
           seenDot = true
         }
@@ -107,28 +108,27 @@ export default function App() {
       }
     }
     if (type === 'number') {
-      if (query.length === 1 && query[0].result) {
-        setQuery([])
+      if (inputHistory.length === 1 && inputHistory[0].result) {
+        setInputHistory([])
       }
     }
-    setQuery((prev) => [...prev, {type, keyName, label}])
+    setInputHistory((prev) => [...prev, { type, keyName, label }])
   }
 
   const evaluateResult = () => {
-    const string = query.reduce((acc, cur) => acc += cur.label, '').replace('×','*').replace('÷','/')
+    const expression = inputHistory.map(item => item.label).join('').replace('×', '*').replace('÷', '/')
     try {
-      return eval(string).toString()
-    } 
-    catch (error) {
+      return eval(expression).toString()
+    } catch (error) {
       return '0'
     }
   }
 
   const stringifyResult = () => {
-    if (query.length === 0) {
+    if (inputHistory.length === 0) {
       return '0'
     }
-    return query.reduce((acc, cur) => acc += cur.label, '')
+    return inputHistory.map(item => item.label).join('')
   }
 
   return (
@@ -140,9 +140,9 @@ export default function App() {
           keyName={keyName}
           label={label}
           onClick={() => handleClick({ type, keyName, label })}
-          ref={element => buttonRefs.current[keyPress] = element}
+          ref={(element) => (buttonRefs.current[keyPress] = element)}
         />
       ))}
     </div>
-  );
+  )
 }
